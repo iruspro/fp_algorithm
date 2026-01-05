6de225c620505d983a001775795b33a2f7de5470
Ruslan Urazbakhtin <ruspro.business@gmail.com>
2026-01-05 11:03:09 +0100

    refactor: add type definitions to Lin_ineq module interface

--- lib/lin_ineq.mli
+++ lib/lin_ineq.mli
@@ -1,5 +1,5 @@
-type rel
-type t
+type rel = LessThan | LessEqual | GreaterThan | GreaterEqual
+type t = Lin_expr.t * Lin_expr.t * rel
 
 val is_satisfied : Point.t -> t -> bool
 val find_unsatisfied : Point.t -> t list -> t option
