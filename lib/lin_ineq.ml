9aefa8605b7443ac7c8a0c1ab733a2fc4b4afc9a
Ruslan Urazbakhtin <ruspro.business@gmail.com>
2026-01-05 12:11:09 +0100

    test: add tests for Lin_ineq module

--- lib/lin_ineq.ml
+++ lib/lin_ineq.ml
@@ -27,3 +27,103 @@ let negate (le1, le2, rel) =
     | GreaterEqual -> LessThan
   in
   (le1, le2, rel)
+
+(* Tests *)
+
+(* is_satisfied *)
+let%test "is_satisfied random" =
+  let result = ref true in
+  let dims = [| 0; 1; 5; 10 |] in
+  let _ =
+    for i = 1 to pred (Array.length dims) do
+      let dim = dims.(i) in
+      let expr1 = List.init (succ dim) (fun _ -> Q.of_int 2)
+      and expr2 = List.init (succ dim) (fun _ -> Q.of_int 1) in
+      let n_cases = 10 in
+      let points =
+        List.init n_cases (fun _ ->
+            List.init dim (fun _ -> Q.of_int (Random.full_int 100)))
+      in
+      let rec aux acc rel = function
+        | [] -> acc
+        | point :: points ->
+            let acc =
+              match rel with
+              | GreaterThan | GreaterEqual ->
+                  acc && is_satisfied point (expr1, expr2, rel)
+              | LessThan | LessEqual ->
+                  acc || is_satisfied point (expr1, expr2, rel)
+            in
+            aux acc rel points
+      in
+      result :=
+        aux true GreaterThan points
+        && aux true GreaterEqual points
+        && (not (aux false LessEqual points))
+        && not (aux false LessThan points)
+    done
+  in
+  !result
+
+let%test "is_satisfied equals" =
+  let expr = List.init 3 (fun _ -> Q.of_int 2)
+  and zero = Lin_expr.zero 2
+  and point = [ Q.of_int (-1); Q.of_int 0 ] in
+  is_satisfied point (expr, zero, LessEqual)
+  && is_satisfied point (expr, zero, GreaterEqual)
+  && (not (is_satisfied point (expr, zero, LessThan)))
+  && not (is_satisfied point (expr, zero, GreaterThan))
+
+(* find_unsatisfied *)
+let%test "find_unsatisfied success" =
+  let zero = Lin_expr.zero 2 in
+  let ineqs =
+    [
+      (List.init 3 (fun _ -> Q.of_int 2), zero, GreaterThan);
+      (List.init 3 (fun _ -> Q.of_int 3), zero, GreaterThan);
+      (List.init 3 (fun _ -> Q.of_int (-2)), zero, GreaterThan);
+      (List.init 3 (fun _ -> Q.of_int 3), zero, GreaterThan);
+      (List.init 3 (fun _ -> Q.of_int (-1)), zero, GreaterThan);
+    ]
+  and point = [ Q.of_int 1; Q.of_int 1 ] in
+  let res = find_unsatisfied point ineqs in
+  match res with None -> false | Some _ -> true
+
+let%test "find_unsatisfied fail" =
+  let zero = Lin_expr.zero 2 in
+  let ineqs =
+    [
+      (List.init 3 (fun _ -> Q.of_int 2), zero, GreaterThan);
+      (List.init 3 (fun _ -> Q.of_int 3), zero, GreaterThan);
+      (List.init 3 (fun _ -> Q.of_int (-2)), zero, LessThan);
+      (List.init 3 (fun _ -> Q.of_int 3), zero, GreaterThan);
+      (List.init 3 (fun _ -> Q.of_int (-1)), zero, LessThan);
+    ]
+  and point = [ Q.of_int 1; Q.of_int 1 ] in
+  let res = find_unsatisfied point ineqs in
+  match res with None -> true | Some _ -> false
+
+(* negate *)
+let%test "negate LessThan -> GreaterEqual" =
+  let expr1 = [ Q.one ] and expr2 = [ Q.zero ] in
+  let input = (expr1, expr2, LessThan) in
+  let e1, e2, r = negate input in
+  r = GreaterEqual && e1 = expr1 && e2 = expr2
+
+let%test "negate LessEqual -> GreaterThan" =
+  let expr1 = [ Q.one ] and expr2 = [ Q.zero ] in
+  let input = (expr1, expr2, LessEqual) in
+  let e1, e2, r = negate input in
+  r = GreaterThan && e1 = expr1 && e2 = expr2
+
+let%test "negate GreaterThan -> LessEqual" =
+  let expr1 = [ Q.one ] and expr2 = [ Q.zero ] in
+  let input = (expr1, expr2, GreaterThan) in
+  let e1, e2, r = negate input in
+  r = LessEqual && e1 = expr1 && e2 = expr2
+
+let%test "negate GreaterEqual -> LessThan" =
+  let expr1 = [ Q.one ] and expr2 = [ Q.zero ] in
+  let input = (expr1, expr2, GreaterEqual) in
+  let e1, e2, r = negate input in
+  r = LessThan && e1 = expr1 && e2 = expr2
