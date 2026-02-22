63d76b1d50b1b8f9b0936d1ba6b6f54b86ac1e7c
Ruslan Urazbakhtin <ruspro.business@gmail.com>
2026-02-22 21:13:27 +0100

    feat: add flip_last, with_expr, to_string, print functions for Cond_lin_expr module

--- lib/linear/cond_lin_expr.ml
+++ lib/linear/cond_lin_expr.ml
@@ -4,36 +4,29 @@ let construct constraints expr = { constraints; expr }
 let constraints cle = cle.constraints
 let expr cle = cle.expr
 
-let dual dim cle =
-  let dual_ineq ineq =
-    let lhs = Lin_ineq.lhs ineq
-    and rhs = Lin_ineq.rhs ineq
-    and rel = Lin_ineq.rel ineq in
+(* Functions *)
+let with_expr cle expr = construct (constraints cle) expr
 
-    (* 1 - x_n *)
-    let expr =
-      Lin_expr.add (Lin_expr.one dim)
-        (Lin_expr.mul_by Q.minus_one (Lin_expr.x dim dim))
-    in
-
-    let lhs = Lin_expr.sub_last lhs expr and rhs = Lin_expr.sub_last rhs expr in
-
-    Lin_ineq.construct dim lhs rhs rel
-  in
-
-  let dual_expr expr =
-    let q_n =
-      match expr with [] -> assert false | [ _ ] -> Q.zero | q_n :: _ -> q_n
-    in
-    let rec aux acc = function
-      | [] -> List.rev acc
-      | [ q_0 ] -> aux (Q.sub (Q.sub Q.one q_0) q_n :: acc) []
-      | q_n :: qs when List.length qs = dim -> aux (q_n :: acc) qs
-      | q :: qs -> aux (Q.neg q :: acc) qs
-    in
-    aux [] expr
-  in
-
-  let constraints = List.map dual_ineq (constraints cle)
-  and expr = Lin_expr.from_list (dual_expr (Lin_expr.as_list (expr cle))) in
+let flip_last cle =
+  let constraints = List.map Lin_ineq.flip_last (constraints cle)
+  and expr = Lin_expr.flip_last (expr cle) in
   construct constraints expr
+
+(* Print *)
+let to_string cle =
+  let ineqs = constraints cle and expr = expr cle in
+
+  let count = List.length ineqs in
+  let constraints =
+    "{\n"
+    ^ List.fold_left ( ^ ) ""
+        (List.mapi
+           (fun i ineq ->
+             if i <> count - 1 then Lin_ineq.to_string ineq ^ "\n"
+             else Lin_ineq.to_string ineq)
+           ineqs)
+    ^ "\n}"
+  in
+  constraints ^ " ⊢ " ^ Lin_expr.to_string expr
+
+let print cle = print_string (to_string cle)
--- lib/linear/cond_lin_expr.mli
+++ lib/linear/cond_lin_expr.mli
@@ -1,11 +1,23 @@
 type t
 
-(* Constructor *)
+(** {2 Constructor} *)
+
 val construct : Lin_ineq.t list -> Lin_expr.t -> t
 
-(* Getters *)
+(** {2 Getters} *)
+
 val constraints : t -> Lin_ineq.t list
 val expr : t -> Lin_expr.t
 
-(* Functions *)
-val dual : int -> t -> t
+(** {2 Functions} *)
+
+val with_expr : t -> Lin_expr.t -> t
+(** [with_expr cle expr] returns a copy of [cle] with its expression replaced by
+    [expr]. *)
+
+val flip_last : t -> t
+
+(** {2 Print} *)
+
+val to_string : t -> string
+val print : t -> unit
