a2054c264e9f026c320f9feffa1226d6aee0803b
Ruslan Urazbakhtin <ruspro.business@gmail.com>
2026-02-23 22:18:37 +0100

    refactor: Mu_term module

--- lib/mu_calc/mu_term.ml
+++ lib/mu_calc/mu_term.ml
@@ -11,50 +11,74 @@ type t =
   | Lwc of t * t (* Lukasiewicz weak conjunction *)
   | Lsd of t * t (* Lukasiewicz strong disjunction *)
   | Lsc of t * t (* Lukasiewicz strong conjunction *)
-  | Mu of t (* lfp *)
-  | Nu of t (* gfp *)
+  | Mu of int * t (* lfp *)
+  | Nu of int * t (* gfp *)
 
-let n_free_vars term =
-  let rec aux_binded b_acc = function
-    | Var _ | Zero | One -> b_acc
-    | Scm (_, term) -> aux_binded b_acc term
-    | Mu term | Nu term -> aux_binded (succ b_acc) term
-    | Lwd (term1, term2)
-    | Lwc (term1, term2)
-    | Lsd (term1, term2)
-    | Lsc (term1, term2) ->
-        aux_binded b_acc term1 + aux_binded 0 term2
-  in
-  let rec aux_vars v_acc = function
-    | Var _ -> v_acc + 1
-    | Zero | One -> v_acc
-    | Scm (_, term) | Mu term | Nu term -> aux_vars v_acc term
-    | Lwd (term1, term2)
-    | Lwc (term1, term2)
-    | Lsd (term1, term2)
-    | Lsc (term1, term2) ->
-        aux_vars v_acc term1 + aux_vars 0 term2
-  in
-  aux_vars 0 term - aux_binded 0 term
+(* Constructors *)
+let var i =
+  if i < 1 then invalid_arg "Mu_term.var: index must be greater or equal than 1"
+  else Var i
+
+let const q =
+  if Q.gt q Q.one || Q.lt q Q.zero then
+    invalid_arg "Mu_term.const: constant must belong to the interval [0,1]"
+  else if Q.equal q Q.zero then Zero
+  else if Q.equal q Q.one then One
+  else Scm (q, One)
+
+let scalar_mult q term =
+  if Q.gt q Q.one || Q.lt q Q.zero then
+    invalid_arg
+      "Mu_term.scalar_mult: constant must belong to the interval [0,1]"
+  else if Q.equal q Q.zero then Zero
+  else if Q.equal q Q.one then term
+  else Scm (q, term)
+
+let weak_disj term1 term2 =
+  match (term1, term2) with
+  | One, _ | _, One -> One
+  | term1, term2 -> Lwd (term1, term2)
+
+let weak_conj term1 term2 =
+  match (term1, term2) with
+  | Zero, _ | _, Zero -> Zero
+  | term1, term2 -> Lwc (term1, term2)
+
+let strong_disj term1 term2 =
+  match (term1, term2) with
+  | term1, Zero -> term1
+  | Zero, term2 -> term2
+  | _ -> Lsd (term1, term2)
+
+let strong_conj term1 term2 =
+  match (term1, term2) with
+  | term1, One -> term1
+  | One, term2 -> term2
+  | _ -> Lsc (term1, term2)
+
+let mu i term = Mu (i, term)
+let nu i term = Nu (i, term)
 
 let rec eval term point =
-  let n_vars = Point.dim point in
+  let dim = Point.dim point in
   match term with
   | Var i ->
-      let x = Lin_expr.x n_vars i in
+      let x = Lin_expr.x dim i in
 
       let ineqs =
         [
-          Lin_ineq.construct (Lin_expr.zero n_vars) x Lin_ineq.LessEqual;
-          Lin_ineq.construct x (Lin_expr.one n_vars) Lin_ineq.LessEqual;
+          Lin_ineq.construct (Lin_expr.zero dim) x Lin_ineq.LessEqual;
+          Lin_ineq.construct x (Lin_expr.one dim) Lin_ineq.LessEqual;
         ]
       in
       Cond_lin_expr.construct ineqs x
-  | Zero -> Cond_lin_expr.construct [] (Lin_expr.zero n_vars)
-  | One -> Cond_lin_expr.construct [] (Lin_expr.one n_vars)
+  (* TODO: Lin_expr.zero and Lin_expr.one without dim *)
+  | Zero -> Cond_lin_expr.construct [] (Lin_expr.zero dim)
+  | One -> Cond_lin_expr.construct [] (Lin_expr.one dim)
   | Scm (q, term) ->
       let cle = eval term point in
 
+      (* TODO: add Cond_lin_expr.mul_by *)
       let constraints = Cond_lin_expr.constraints cle
       and expr = Cond_lin_expr.expr cle in
       Cond_lin_expr.construct constraints (Lin_expr.mul_by q expr)
@@ -96,7 +120,7 @@ let rec eval term point =
       and constraints2 = Cond_lin_expr.constraints cle2
       and expr2 = Cond_lin_expr.expr cle2 in
 
-      let expr = Lin_expr.add expr1 expr2 and one = Lin_expr.one n_vars in
+      let expr = Lin_expr.add expr1 expr2 and one = Lin_expr.one dim in
       let ineq = Lin_ineq.construct expr one Lin_ineq.LessEqual in
       if Lin_ineq.is_satisfied ineq point then
         Cond_lin_expr.construct ((ineq :: constraints1) @ constraints2) expr
@@ -114,8 +138,8 @@ let rec eval term point =
 
       let expr =
         Lin_expr.add (Lin_expr.add expr1 expr2)
-          (Lin_expr.mul_by Q.minus_one (Lin_expr.one n_vars))
-      and zero = Lin_expr.zero n_vars in
+          (Lin_expr.mul_by Q.minus_one (Lin_expr.one dim))
+      and zero = Lin_expr.zero dim in
       let ineq = Lin_ineq.construct expr zero Lin_ineq.GreaterEqual in
       if Lin_ineq.is_satisfied ineq point then
         Cond_lin_expr.construct ((ineq :: constraints1) @ constraints2) expr
@@ -123,26 +147,41 @@ let rec eval term point =
         Cond_lin_expr.construct
           ((Lin_ineq.reverse ineq :: constraints1) @ constraints2)
           zero
-  | Mu term -> Algorithm.lfp (eval term) point
-  | Nu term -> Algorithm.gfp (eval term) point
+  | Mu (_, term) -> Algorithm.lfp (eval term) point
+  | Nu (_, term) -> Algorithm.gfp (eval term) point
+
+(* Print *)
+let term_to_op = function
+  | Var _ | Zero | One | Scm (_, _) -> ""
+  | Lwd (_, _) -> " ⊔ "
+  | Lwc (_, _) -> " ⊓ "
+  | Lsd (_, _) -> " ⊕ "
+  | Lsc (_, _) -> " ⊙ "
+  | Mu (i, _) -> "μx" ^ Pprint.int_to_subscript i ^ "."
+  | Nu (i, _) -> "vx" ^ Pprint.int_to_subscript i ^ "."
 
 let rec to_string term =
+  let bracket b = function
+    | Var _ | Zero | One | Scm (_, Zero) | Scm (_, One) | Scm (_, Var _) -> ""
+    | _ -> b
+  in
+  let left_bracket = bracket "(" in
+  let right_bracket = bracket ")" in
+  let op = term_to_op term in
   match term with
   | Var i -> "x" ^ Pprint.int_to_subscript i
-  | Zero -> "0"
+  | Zero | Scm (_, Zero) -> "0"
   | One -> "1"
+  | Scm (q, One) -> Q.to_string q
   | Scm (q, term) ->
       let s = Q.to_string q in
-      s ^ "(" ^ to_string term ^ ")"
-  | Lwd (term1, term2) ->
-      "(" ^ to_string term1 ^ ") ⊔ (" ^ to_string term2 ^ ")"
-  | Lwc (term1, term2) ->
-      "(" ^ to_string term1 ^ ") ⊓ (" ^ to_string term2 ^ ")"
-  | Lsd (term1, term2) ->
-      "(" ^ to_string term1 ^ ") ⊕ (" ^ to_string term2 ^ ")"
+      s ^ " " ^ left_bracket term ^ to_string term ^ right_bracket term
+  | Lwd (term1, term2)
+  | Lwc (term1, term2)
+  | Lsd (term1, term2)
   | Lsc (term1, term2) ->
-      "(" ^ to_string term1 ^ ") ⊙ (" ^ to_string term2 ^ ")"
-  | Mu term -> "μy.(" ^ to_string term ^ ")"
-  | Nu term -> "vy.(" ^ to_string term ^ ")"
+      left_bracket term1 ^ to_string term1 ^ right_bracket term1 ^ op
+      ^ left_bracket term2 ^ to_string term2 ^ right_bracket term2
+  | Mu (_, term) | Nu (_, term) -> op ^ to_string term
 
 let print term = print_string (to_string term)
--- lib/mu_calc/mu_term.mli
+++ lib/mu_calc/mu_term.mli
@@ -1,18 +1,59 @@
 open Linear
 
-type t =
-  | Var of int (* x_i *)
-  | Zero
-  | One
-  | Scm of Q.t * t (* scalar multiplication *)
-  | Lwd of t * t (* Lukasiewicz weak disjunction *)
-  | Lwc of t * t (* Lukasiewicz weak conjunction *)
-  | Lsd of t * t (* Lukasiewicz strong disjunction *)
-  | Lsc of t * t (* Lukasiewicz strong conjunction *)
-  | Mu of t (* lfp *)
-  | Nu of t (* gfp *)
+type t
+(** Łukasiewicz μ-term *)
 
-val n_free_vars : t -> int
+(** {2 Constructors} *)
+
+val var : int -> t
+(** [var i] returns the μ-term corresponding to the variable [x_i].
+
+    Raise [Invalid_argument] if [i < 1]. *)
+
+val const : Q.t -> t
+(** [const q] returns the μ-term equal to [q].
+
+    Raise [Invalid_argument] if [q] is not in the interval [[0,1]]. *)
+
+val scalar_mult : Q.t -> t -> t
+(** [scalar_mult q term] returns the μ-term obtained by multiplying [term] by
+    the scalar coefficient [q].
+
+    Raise [Invalid_argument] if [q] is not in the interval [[0,1]]. *)
+
+val weak_disj : t -> t -> t
+(** [weak_disj term1 term2] returns the Łukasiewicz weak disjunction of [term1]
+    and [term2]. *)
+
+val weak_conj : t -> t -> t
+(** [weak_conj term1 term2] returns the Łukasiewicz weak conjunction of [term1]
+    and [term2]. *)
+
+val strong_disj : t -> t -> t
+(** [strong_disj term1 term2] returns the Łukasiewicz strong disjunction of
+    [term1] and [term2]. *)
+
+val strong_conj : t -> t -> t
+(** [strong_conj term1 term2] returns the Łukasiewicz strong conjunction of
+    [term1] and [term2]. *)
+
+val mu : int -> t -> t
+(** [mu x_index term] constructs the least fix point term μx.term, where
+    [x_index] denotes the bound variable.
+
+    Raise [Invalid_argument] if [i < 1]. *)
+
+val nu : int -> t -> t
+(** [nu x_index term] constructs the greatest fix point term vx.term, where
+    [x_index] denotes the bound variable.
+
+    Raise [Invalid_argument] if [i < 1]. *)
+
+(** {2 Functions} *)
+
 val eval : t -> Point.t -> Cond_lin_expr.t
+
+(** {2 Print} *)
+
 val to_string : t -> string
 val print : t -> unit
