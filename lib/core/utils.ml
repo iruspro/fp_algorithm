6d4e8b94bca54368d84ddd8a35a82a843dd5aad6
Ruslan Urazbakhtin <ruspro.business@gmail.com>
2026-03-01 14:45:28 +0100

    fix: gfp dual

--- lib/core/utils.ml
+++ lib/core/utils.ml
@@ -1,24 +1,30 @@
 open Linear
 
-let complement expr = Lin_expr.sub (Linear.Lin_expr.const Q.one) expr
+let complement expr = Lin_expr.sub (Lin_expr.const Q.one) expr
 
-(* (** [flip_expr expr i] performs the substitution [x_i -> 1 - x_i] in the linear
-    expression [expr].
+(** f(x_1, ..., x_n) -> f(x_1, ..., 1 - x_n) *)
+let flip_last local_alg point =
+  let n = Point.dim point in
+  let sub = Lin_expr.sub (Lin_expr.const Q.one) (Lin_expr.x n) in
 
-    Raise [Invalid_argument] if [i < 1]. *)
-let flip_expr expr i =
-  if i < 1 then invalid_arg "Utils.flip_expr: variable index must be >= 1"
-  else
-    let expr' = Lin_expr.sub (Lin_expr.const Q.one) (Lin_expr.x i) in
-    Lin_expr.substitute expr i expr' *)
+  let flip_expr expr = Lin_expr.substitute expr n sub in
+  let flip_ineq ineq = Lin_ineq.substitute ineq n sub in
 
-let flip_last _ _ = failwith ""
-(* let point = Point.flip_last point in *)
-(* Cond_lin_expr.flip_last (local_alg point) *)
+  let cle = local_alg point in
+  let constraints = List.map flip_ineq (Cond_lin_expr.constraints cle)
+  and expr = flip_expr (Cond_lin_expr.expr cle) in
+  Cond_lin_expr.construct constraints expr
 
-let complement_ _ _ = failwith ""
-(* let cle = local_alg point in
-  let expr = Lin_expr.complement (Cond_lin_expr.expr cle) in
-  Cond_lin_expr.with_expr cle expr *)
+(** f -> 1 - f *)
+let flip local_alg (point : Point.t) =
+  let cle = local_alg point in
+  Cond_lin_expr.with_expr cle (complement (Cond_lin_expr.expr cle))
 
-let dual local_alg = complement_ (flip_last local_alg)
+let dual local_alg point =
+  let flip_point point =
+    match Point.as_reversed_list point with
+    | [] -> point
+    | r_n :: rs -> Point.from_list (Q.sub Q.one r_n :: rs)
+  in
+  let point = flip_point point in
+  flip (flip_last local_alg) point
--- lib/core/utils.mli
+++ lib/core/utils.mli
@@ -2,6 +2,6 @@ open Linear
 
 val complement : Lin_expr.t -> Lin_expr.t
 (** [complement expr] returns the linear expression obtained by subtracting
-    [expr] from the constant expression equal to [1]. *)
+    [expr] from the constant expression equal to {m 1}. *)
 
 val dual : Local_alg.t -> Local_alg.t
